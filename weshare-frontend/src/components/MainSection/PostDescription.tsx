31f60798a546aa68373c6d3c823ad0f6574da381
HNamm <80162506+huydre@users.noreply.github.com>
2023-10-21 01:33:07 +0700

    fix multi line in description post

--- weshare-frontend/src/components/MainSection/PostDescription.tsx
+++ weshare-frontend/src/components/MainSection/PostDescription.tsx
@@ -1,4 +1,8 @@
 export const PostDescription = ({ description }: { description: string }) => {
-    return <p className="my-[5px] text-[15px] text-black dark:text-white">{description}</p>;
+    return (
+        description.split('\n').map((line) => (
+            <p className="my-[5px] text-[15px] text-black dark:text-white">{line}</p>
+        ))
+    )
  };
  
